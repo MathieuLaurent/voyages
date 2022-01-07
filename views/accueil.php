c4b29f7a93a0f9bba1bce33ecd390837deab721d
Laurent Mathieu <mathieu.laurent1995@gmail.com>
2022-01-07 17:33:56 +0100

    lien voyage

--- views/accueil.php
+++ views/accueil.php
@@ -33,7 +33,7 @@
                     Dropdown
                 </a>
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="?action=listVoyage">Liste Voyage</a></li>
                     <li><a class="dropdown-item" href="#">Another action</a></li>
                     <li><hr class="dropdown-divider"></li>
                     <li><a class="dropdown-item" href="#">Something else here</a></li>
