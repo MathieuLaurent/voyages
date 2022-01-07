5e1d48d3a3c2e879c4f6365852b2e59d850e1b85
Laurent Mathieu <mathieu.laurent1995@gmail.com>
2022-01-07 16:15:18 +0100

    model

---
+++ models/Voyage.php
@@ -0,0 +1,16 @@
+<?php 
+
+class Voyage{
+
+    private $ref;
+    private $nom;
+    private $accroche;
+    private $prix;
+    private $duree;
+    private $img1;
+    private $img2;
+    private $img3;
+
+}
+
+?>
