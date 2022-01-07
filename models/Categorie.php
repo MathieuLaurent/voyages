044b3c1ad2c61472c212677343b3d8b99e2c3a6e
Lee <95095368+JulienB63100@users.noreply.github.com>
2022-01-07 17:15:19 +0100

    Ajout

--- models/Categorie.php
+++ models/Categorie.php
@@ -1,6 +1,48 @@
 <?php
 
 class Categorie{
+  
+    private $_ref;
+    private $_nom;
+    private $_description;
+    
+    public function __construct(string $ref, string $nom, string $description){
+
+        $this -> ref = $ref;
+        $this -> nom = $nom;
+        $this -> description;
+    }
+
+    public function setRef($ref){
+        $this -> ref = ref;
+        return $this;
+
+    }
+    
+    public function getRef(){
+        return $this -> ref;
+    }
+
+    public function setNom($nom){
+        $this -> nom = nom;
+        return $this;
+
+    }
+    
+    public function getNom(){
+        return $this -> nom;
+    }
+
+    public function setDescription($descr){
+        $this -> description = description;
+        return $this;
+
+    }
+    
+    public function getDescription(){
+        return $this -> description;
+    }
+        
     
 }
 
